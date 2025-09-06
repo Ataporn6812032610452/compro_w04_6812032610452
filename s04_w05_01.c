52be514d23b8ac0df3671affa33ed0241bb4d464
Ataporn6812032610452 <s6812032610452@email.kmutnb.ac.th>
2025-09-06 11:41:59 +0700

    Update s04_w05_01.c

--- s04_w05_01.c
+++ s04_w05_01.c
@@ -2,11 +2,12 @@
 
 int main(){
     char name = 'B' ; // ค่าชื่อผู้ใช้
-    int age = 18;   // อายุ
-    float weight = 53.9; // น้ำหนัก
+    int age = 19;   // อายุ
+    float weight = 59.7; // น้ำหนัก
 
 
     printf("Student %c is %d years old.\n", name, age); //เเสดงผลข้อมูล ชื่อ อายุ
     printf("his weight is %.1f kg.\n", weight); //เเสดงผลข้อมูล น้ำหนัก
     return 0; //คืนค่า 0 เพื่อบอกว่าการทำงานสำเร็จ
+
 }
